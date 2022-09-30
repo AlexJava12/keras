20e1f743eaa6998adb32370035d1e85905e0309e
iyervanugopal9999 <91649338+iyervanugopal9999@users.noreply.github.com>
2022-10-01 01:20:52 +0530

    Fibonacci sequence updated

--- indexing.py
+++ indexing.py
@@ -1,3 +1,9 @@
 print("Hello Yellow")
 for i in 10:
   print("change this text")
+  
+  def fibonacci(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
